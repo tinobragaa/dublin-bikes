0f961dd7fdb07120e6c967768ecbfa49ae3c0b21
Valentino Braga <valentinobragaa@gmail.com>
2025-02-20 04:44:20 +0000

    Fix: fixed URL structure for index.html to /home

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def landing():
+def index():
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 
