2a7af1519a93fb8790ca0535f0bd46b8bb62f51b
Valentino Braga <valentinobragaa@gmail.com>
2025-04-10 07:58:38 +0100

    Add: added nav and footer to sign in page and final style

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email, EqualTo
 
+
 class SignupForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
@@ -10,7 +11,8 @@ class SignupForm(FlaskForm):
     ])
     submit = SubmitField('Sign Up')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
-    submit = SubmitField('Log In')
+    submit = SubmitField('Log in')
